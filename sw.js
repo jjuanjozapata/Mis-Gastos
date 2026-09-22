950df116cb1747d5e3aa834529ef3ffbb41c8bcf
jjuanjozapata <jjuajozapata@gmail.com>
2026-09-22 11:38:29 -0500

    actualizacion

--- sw.js
+++ sw.js
@@ -4,7 +4,6 @@ const ASSETS = [
     '/',
     '/index.html',
     '/manifest.json',
-    'https://cdn.tailwindcss.com',
     'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.3/dist/umd/supabase.min.js',
     'https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.js',
     'https://cdn.jsdelivr.net/npm/dompurify@3.0.6/dist/purify.min.js'
