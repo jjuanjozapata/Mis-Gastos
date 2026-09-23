5331016cd5ce3f4ab4b8497b7d5c06abf0c22ec4
jjuanjozapata <jjuajozapata@gmail.com>
2026-09-23 17:29:37 -0500

    actualizacion

--- public/sw.js
+++ public/sw.js
@@ -12,7 +12,9 @@ const ASSETS = [
 self.addEventListener('install', event => {
     self.skipWaiting();
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+            .catch(err => console.error('Fallo en SW al cachear ASSETS:', err))
     );
 });
 
